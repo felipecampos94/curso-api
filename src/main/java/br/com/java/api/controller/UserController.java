14edfce022bb6174270646e2badad13732377099
Felipe Campos <felipecamposlv@gmail.com>
2023-02-06 13:00:10 -0300

    Retornando DTO

--- src/main/java/br/com/java/api/controller/UserController.java
+++ src/main/java/br/com/java/api/controller/UserController.java
@@ -1,8 +1,9 @@
 package br.com.java.api.controller;
 
-import br.com.java.api.domain.User;
+import br.com.java.api.domain.dto.UserDTO;
 import br.com.java.api.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +17,10 @@ public class UserController {
 
     private final UserService userService;
 
+    private final ModelMapper mapper;
+
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(userService.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(mapper.map(userService.findById(id), UserDTO.class));
     }
 }
