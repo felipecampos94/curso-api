e97d19c7a2b689e3f19a64b7c24ccfaf79a77c6c
Felipe Campos <felipecamposlv@gmail.com>
2023-02-06 11:06:51 -0300

    Criação da classe User

---
+++ src/main/java/br/com/java/api/domain/User.java
@@ -0,0 +1,25 @@
+package br.com.java.api.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
