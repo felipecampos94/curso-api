38531ec7203a2fd920dfd12ca51937362282d32d
Felipe Campos <felipecamposlv@gmail.com>
2023-02-06 11:13:28 -0300

    Criação UserController

---
+++ src/main/java/br/com/java/api/controller/UserController.java
@@ -0,0 +1,18 @@
+package br.com.java.api.controller;
+
+import br.com.java.api.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().build();
+    }
+}
